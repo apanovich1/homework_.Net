624c788f2ecd3f507788d70b5abba78020a949f1
ann apanovich <anna.apanovich1@gmail.com>
2021-11-03 15:51:54 +0300

    adding files

---
+++ source/repos/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] socks = new string[6] { "A", "B", "C", "A", "B", "D" };
+            int result = 0;
+            foreach (string sock in socks)
+            if (socks[0] == socks[1])
+                {
+                    result++;
+                }
+            if (socks[0] == socks[2])
+            {
+                result++;
+            }
+            if (socks[0] == socks[3])
+            {
+                result++;
+            }
+            if (socks[0] == socks[4])
+            {
+                result++;
+            }
+            if (socks[0] == socks[5])
+            {
+                result++;
+            }
+            if (socks[1] == socks[2])
+            {
+                result++;
+            }
+            if (socks[1] == socks[3])
+            {
+                result++;
+            }
+            if (socks[1] == socks[4])
+            {
+                result++;
+            }
+            if (socks[1] == socks[5])
+            {
+                result++;
+            }
+            if (socks[2] == socks[3])
+            {
+                result++;
+            }
+            if (socks[2] == socks[4])
+            {
+                result++;
+            }
+            if (socks[2] == socks[5])
+            {
+                result++;
+            }
+            if (socks[3] == socks[4])
+            {
+                result++;
+            }
+            if (socks[3] == socks[5])
+            {
+                result++;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
+
