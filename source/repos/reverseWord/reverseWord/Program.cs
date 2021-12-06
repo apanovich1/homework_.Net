2035004bb53c7fe792c5768f0d29636605a14b16
ann apanovich <anna.apanovich1@gmail.com>
2021-12-06 22:37:59 +0300

    'reverseWord'

---
+++ source/repos/reverseWord/reverseWord/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class Program
+{
+    public static void Main()
+    {
+
+        int number = 123;
+        string numberConverted = Convert.ToString(number);
+        string result = new string(numberConverted.ToCharArray().Reverse().ToArray());
+        
+        Console.WriteLine(result + number);
+
+    }
+}
