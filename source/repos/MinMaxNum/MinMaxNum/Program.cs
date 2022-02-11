8c7834104c84d9c05dca5b5bb9e696cde2d0471d
ann apanovich <anna.apanovich1@gmail.com>
2022-02-11 23:44:13 +0300

    MinMaxNum

---
+++ source/repos/MinMaxNum/MinMaxNum/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main()
+    {
+        var numbers = new int[] { 1, 213124, 54, 4565676 };
+        int maxValue = numbers.Max();
+        int minValue = numbers.Min();
+        Console.WriteLine($"minimum number = {minValue} \nmaximum number = {maxValue}");
+    }
+
+}
