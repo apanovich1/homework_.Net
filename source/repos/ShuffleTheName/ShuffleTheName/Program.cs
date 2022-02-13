1316936d360a6e09ecd844690e9ba7ecf3a064b5
ann apanovich <anna.apanovich1@gmail.com>
2022-02-14 00:01:36 +0300

    ShuffleTheName

---
+++ source/repos/ShuffleTheName/ShuffleTheName/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class SamplesArray1
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your full name: ");
+        string text = Console.ReadLine();
+        string[] textSplit = text.Split(" ");
+        string firstWord = textSplit.First();
+        string secondWord = textSplit.Last();
+        string Items = secondWord + ' ' + firstWord;
+        Console.WriteLine(Items);
+    }
+}
