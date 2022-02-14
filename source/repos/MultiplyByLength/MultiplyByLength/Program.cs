d7985bab47dd09f9f12393a21fe783d59b56be05
ann apanovich <anna.apanovich1@gmail.com>
2022-02-14 21:35:39 +0300

    MyltiplyByLength

---
+++ source/repos/MultiplyByLength/MultiplyByLength/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 0, 3, 3, 7, 2, 1 };
+        int length = numbers.Length;
+        var result = numbers.Select(n => n * length);
+        foreach (var item in result)
+        {
+            Console.WriteLine(item);
+        }
+
+    }
+}
