d26a043f1a6791a03a07435d71e53b083734f861
ann apanovich <anna.apanovich1@gmail.com>
2022-02-21 21:33:57 +0300

    FibonacciNumber

---
+++ FibonacciNumber/FibonacciNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+class FibonacciNumber
+{
+    public static int Fib(BigInteger num)
+    {
+        // int num =
+        // int num = ulong (n);
+       // BigInteger assignedFromLong = num;
+     //  num = new BigInteger(number);
+        if (num <= 1)
+        {
+            return num;
+        }
+        else
+        {
+            return Fib(num - 1) + Fib(num - 2);
+        }
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Insert your number (0-200): ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(Fib(num));
+       // BigInteger
+           // Convert.ToInt32
+    }
+
+}
+//long l=Long.parseLong(str);
