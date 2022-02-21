c56120ab18276ab849340c6ececa1402fe6e4ace
ann apanovich <anna.apanovich1@gmail.com>
2022-02-22 00:12:08 +0300

    CensoredStrings

---
+++ CensoredStrings/CensoredStrings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class CensoredString
+{
+    public static void Main()
+    {
+        string input = "Wh*r* d*d my v*w*ls g*?";
+    }
+    public string  ReplaceVowels(string input, string withVowels )
+    {
+        string pattern = "[eeioeo]";
+        Regex regex = new Regex(pattern);
+        return regex.Replace(input, withVowels);
+    }
+}
