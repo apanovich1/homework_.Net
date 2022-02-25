ab0693af5e58bdffbf1af8573529d04f161372f0
ann apanovich <anna.apanovich1@gmail.com>
2022-02-25 22:57:10 +0300

    ValidHexCode

---
+++ ValidHexCode/ValidHexCode/Program.cs
@@ -0,0 +1,24 @@
+﻿/*A hex code must begin with a pound key # and is exactly 6 characters in length.
+Each character must be a digit from 0-9 or an alphabetic character from A-F.
+All alphabetic characters may be uppercase or lowercase.*/
+
+using System.Text.RegularExpressions;
+
+class ValidHexCode
+{
+    static void Main()
+    {
+        Console.WriteLine("Insert your Hexcode: ");
+        var input = Console.ReadLine();
+
+        var reg = new Regex(@"^#([0-9a-fA-F]){6}$");
+        if (!reg.IsMatch(input))
+        {
+            Console.WriteLine($"{input} is invalid");
+        }
+        else
+        {
+            Console.WriteLine($"{input} is valid");
+        }
+    }
+}
