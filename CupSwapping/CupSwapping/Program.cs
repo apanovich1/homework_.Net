d60f83960622571a192251606164707e222a4137
ann apanovich <anna.apanovich1@gmail.com>
2022-03-23 23:44:22 +0500

    CupSwapping

---
+++ CupSwapping/CupSwapping/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Create a function that returns the letter position that the ball is at, once I finish swapping the cups.
+The swaps will be given to you as an array.
+A swap could be notated in two different ways, since both ways end up with the same outcome.
+All swaps will be notated as capital letters and will be valid.
+You cannot swap a cup with itself.*/
+
+class Cups
+{
+    static void Main()
+    {
+        var currentPosition = "b";
+
+        Console.WriteLine("Insert your swaps: ");
+        var movementsInput = Console.ReadLine();
+
+        if (movementsInput == null)
+        {
+            throw new Exception();
+        }
+        var movements = movementsInput.Split(" ").ToList();
+
+        foreach (var move in movements)
+        {
+            if (move.Contains(currentPosition))
+            {
+                var nextPosition = move.Replace(currentPosition, "");
+                currentPosition = nextPosition;
+            }
+            if (!move.Contains(currentPosition))
+            {
+                continue;
+            }
+        }
+        Console.WriteLine(currentPosition);
+    }
+}
