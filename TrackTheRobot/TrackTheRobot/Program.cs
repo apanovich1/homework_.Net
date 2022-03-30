f1ef92af4b887776887f6bad4f1921784cccdd15
ann apanovich <anna.apanovich1@gmail.com>
2022-03-30 10:30:53 +0500

    TrackTheRobot

---
+++ TrackTheRobot/TrackTheRobot/Program.cs
@@ -0,0 +1,34 @@
+﻿
+class Robot
+{
+    public static void Main()
+    {
+        Console.WriteLine("Insert your moves: ");
+        var movements = Console.ReadLine();
+        var x = 0;
+        var y = 0;
+        foreach (var move in movements)
+        {
+            var word = movements.Split(' ');
+            var number = Int32.Parse(word[1]);
+
+            if (word[0] == "right")
+            {
+                x -= number;
+            }
+            if(word[0] == "left")
+            {
+                x += number;
+            }
+            else if(word[0] == "up")
+            {
+                y += number;
+            }
+            else
+            {
+                y -= number;
+            }
+            Console.WriteLine(x + y);
+        }
+    }
+}
