e9d4cbdc972d745fbd348085db9d87d06109e17d
ann apanovich <anna.apanovich1@gmail.com>
2022-05-18 15:07:50 +0500

    FruitSmoothie

---
+++ FruitSmoothie/FruitSmoothie/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace FruitSmoothie
+{
+    public static class Program
+    {
+        static void Main()
+        {
+            Smoothies mySmoothie = new Smoothies(new string[] { "Strawberries" });
+            Console.WriteLine(mySmoothie.GetCost().ToString());
+            Console.WriteLine(mySmoothie.GetPrice().ToString());
+            Console.WriteLine(mySmoothie.GetName().ToString());
+        }
+    }
+}
+
+//class smoothie в отдельный проект
---
+++ FruitSmoothie/FruitSmoothie/Smoothies.cs
@@ -0,0 +1,83 @@
+﻿namespace FruitSmoothie
+{
+    public class Smoothies
+    {
+        public static Dictionary<string, decimal> Prices = new()
+        {
+            { "Strawberries", 1.50m },
+            { "Banana", 0.50m },
+            { "Mango", 2.50m },
+            { "Blueberries", 1.00m },
+            { "Raspberries", 1.00m },
+            { "Apple", 1.75m },
+            { "Pineapple", 3.50m }
+        };
+
+        public string[] Ingredients { get; set; }
+
+        public Smoothies(string[] ingredients)
+        {
+            Ingredients = ingredients;
+        }
+
+        public decimal GetCost()
+        {
+            var sum = 0m;
+            foreach (var cost in Ingredients)
+            {
+                sum += Prices[cost];
+            }
+            return sum;
+        }
+
+        public decimal GetPrice()
+        {
+            var finalSum = 0m;
+            finalSum = GetCost() * 2.5m;
+            return finalSum;
+        }
+
+        public string GetName()
+        {
+            var name = "";
+
+            Array.Sort(Ingredients);
+
+            for (var ingridient = 0; ingridient < Ingredients.Length; ingridient++)
+            {
+                if (Ingredients[ingridient] == "Strawberries")
+                {
+                    name += "Strawberry ";
+                }
+
+                else if (Ingredients[ingridient] == "Blueberries")
+                {
+                    name += "Blueberry ";
+                }
+
+                else if (Ingredients[ingridient] == "Raspberries")
+                {
+                    name += "Raspberry ";
+                }
+
+                else
+                {
+                    name += Ingredients[ingridient] + " ";
+                }
+
+            }
+
+            if (Ingredients.Length == 1)
+            {
+                name += "Smoothie";
+            }
+
+            else
+            {
+                name += "Fusion";
+            }
+
+            return name;
+        }
+    }
+}
